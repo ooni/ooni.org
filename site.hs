5161ddf86613ee2c2f590fe9e2d14f81b6d1c296
Duncan <duncan@vtllf.org>
2015-04-22 12:56:00 +1200

    Template the test documentation page

--- site.hs
+++ site.hs
@@ -1,25 +1,31 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
+import Data.Monoid (mappend)
+import Hakyll
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
-    match ("css/**" .||. "fonts/**" .||. "images/**" ) $ do
-        route   idRoute
-        compile copyFileCompiler
+  match ("css/**" .||. "fonts/**" .||. "images/**" ) $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match ("index.html" .||. "tests/*.html") $ do
-        route   idRoute
-        compile copyFileCompiler
+  match "index.html" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match "templates/*" $ compile templateCompiler
+  match "tests/*.html" $ do
+    route   idRoute
+    compile $ getResourceBody
+      >>= loadAndApplyTemplate "templates/documentation.html" defaultContext
+      >>= loadAndApplyTemplate "templates/default.html"       defaultContext
+
+  match "templates/*" $ compile templateCompiler
 
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+  dateField "date" "%B %e, %Y" `mappend`
+  defaultContext
