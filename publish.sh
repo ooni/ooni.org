c22edb69b9781ede3edc211a8c2547d640367659
Arturo Filastò <art@fuffa.org>
2015-05-02 16:21:36 +0200

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Must run this before:
 # git remote add ghpage git@github.com:ooni/ooni.github.io.git
-hugo --theme=ooni --buildDrafts --baseUrl="http://ooni.io/"
+OONI_PROBE_REPO_DIR="../ooni-probe/"
+CWD=`pwd`
+hugo --theme=ooni --buildDrafts
+cd $OONI_PROBE_REPO_DIR/docs/
+make clean
+make html
+cd $CWD
+cp -R $OONI_PROBE_REPO_DIR/docs/build/html/ public/docs/
+touch public/.nojekyll
 ghp-import public
 git push -u ghpage gh-pages:master --force
