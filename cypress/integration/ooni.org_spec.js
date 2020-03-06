d29517a513e7e3984d3de5915bde010b86c9f13d
Arturo Filastò <arturo@filasto.net>
2020-03-06 16:11:11 +0100

    Make the test trigger errors on image loading

--- cypress/integration/ooni.org_spec.js
+++ cypress/integration/ooni.org_spec.js
@@ -8,7 +8,21 @@ describe('The Posts', function() {
   const allPosts = JSON.parse(Cypress.env('allPosts'))
   allPosts.map(uri => {
     it(`${uri} successfully loads`, () => {
-      cy.visit(uri)
+       cy.visit(uri, {
+        onLoad: (win) => {
+          win.onerror = cy.onUncaughtException
+        }
+      })
+      // cy.waitForResource('.png')
+      cy.get('img')
+        .should('be.visible')
+        .each(($img) => {
+            expect(
+              $img[0].naturalWidth,
+              `${$img[0].src} is big enough`
+            ).to.be.greaterThan(0)
+        })
+
     })
   })
 })
