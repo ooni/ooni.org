95f36c521de4f491ebb86763e65de441174f7985
Arturo Filastò <arturo@filasto.net>
2023-12-22 15:52:32 +0100

    Separate timestamp script

--- scripts/build_website.sh
+++ scripts/build_website.sh
@@ -12,5 +12,3 @@ cp static/googlec8ce605468a38232.html public/
 cp static/robots.txt public/
 cp _redirects public/
 touch public/.nojekyll
-# netlify does not permit `.web.mtime` filename for unknown reason
-git show HEAD -q '--format=# HELP ooni_web_mtime UNIX Time of the commit used to build website.%n# TYPE ooni_web_mtime gauge%nooni_web_mtime %ct' > public/_web.mtime.txt
